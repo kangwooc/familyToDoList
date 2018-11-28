fd3a92f9434bb222102642879a00f5a0448ca2d1
juan3674 <juan3674@uw.edu>
2018-11-28 15:40:04 -0800

    updated siginIn/signUp

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom"
+import { ROUTES } from "./constants";
+import SignInView from "./SignIn";
+import SignUpView from "./SignUp";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+            <Switch>
+            <Route path={ROUTES.signIn} component={SignInView} />
+            <Route path={ROUTES.signUp} component={SignUpView} />
+            <Redirect to={ROUTES.signIn} />
+            </Switch>
+      </Router>
     );
   }
 }
