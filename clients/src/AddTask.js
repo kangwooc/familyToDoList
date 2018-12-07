d294076a017988ffac48ca63d67871c8451d00c5
juan3674 <juan3674@uw.edu>
2018-12-06 16:12:15 -0800

    juan

--- clients/src/AddTask.js
+++ clients/src/AddTask.js
@@ -2,142 +2,196 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ROUTES } from "./constants";
 
-
 export default class AddTaskView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             task: "",
             socket: null
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
         let auth = localStorage.getItem("auth");
         let url = "wss://localhost:443/ws?auth=" + auth;
-        
+
         this.socket = new WebSocket(url);
-        this.setState({socket : this.socket});
+        this.setState({ socket: this.socket });
         this.socket.onopen = () => {
-          console.log("Connection Opened");
+            console.log("Connection Opened");
         };
-    
+
         this.socket.onclose = () => {
-          console.log("Connection Closed");
+            console.log("Connection Closed");
         };
-      }
+    }
     componentDidUpdate() {
-        this.state.socket.onmessage = (msg) => {
-          console.log("Message received " + msg.data);
+        this.state.socket.onmessage = msg => {
+            console.log("Message received " + msg.data);
         };
-      }
+    }
 
     handleSignOut() {
         fetch("https://localhost:443/sessions/mine", {
             method: "DELETE",
             headers: {
-                "Authorization": localStorage.getItem("auth")
+                Authorization: localStorage.getItem("auth")
             }
-        }).then(res => {
-            if (!res.ok) {
-                throw Error(res.statusText + " " + res.status);
-            }
-            localStorage.clear()
-            this.props.history.push({ pathname: '/signin' })
-        }).catch(function (error) {
-            localStorage.clear()
         })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error(res.statusText + " " + res.status);
+                }
+                localStorage.clear();
+                this.props.history.push({ pathname: "/signin" });
+            })
+            .catch(function(error) {
+                localStorage.clear();
+            });
     }
 
     componentWillMount() {
         fetch(`https://localhost:443/tasks/${this.props.match.params.id}`, {
             method: "GET",
             headers: {
-                "Authorization": window.localStorage.getItem("auth")
+                Authorization: window.localStorage.getItem("auth")
             }
-        }).then(res => {
-            if (!res.ok) { 
-                throw Error(res.statusText + " " + res.status);
-            }
-            return res.json()
-        }).then(data => {
-            console.log(data)
-            let users = data.map((info) => {
-                console.log(info.isProgress)
-                if (info.isProgress) {
-                    this.setState({progress: "Progressing"})
-                } else {
-                    this.setState({progress: "Not Assigned"})
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error(res.statusText + " " + res.status);
                 }
-                return (
-                    <div className="row">
-                        <div className="username col-md-4">
-                            <p>{info.description}</p>
-                            <button className="btn btn-warning my-2 my-sm-0 pull-right" onClick={() => this.handleProgress(info._id)} disabled={info.progress}>
-                                {this.state.progress}
-                            </button>
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                let users = data.map(info => {
+                    console.log(info.isProgress);
+                    if (info.isProgress) {
+                        this.setState({ progress: "Progressing" });
+                    } else {
+                        this.setState({ progress: "Not Assigned" });
+                    }
+                    return (
+                        <div className="row">
+                            <div className="username col-md-4">
+                                <p>{info.description}</p>
+                                <button
+                                    className="btn btn-warning my-2 my-sm-0 pull-right"
+                                    onClick={() =>
+                                        this.handleProgress(info._id)
+                                    }
+                                    disabled={info.progress}
+                                >
+                                    {this.state.progress}
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                );
+                    );
+                });
+                this.setState({ data: users });
+            })
+            .catch(error => {
+                alert(error);
+                localStorage.clear();
+                this.props.history.push({ pathname: "/signin" });
             });
-            this.setState({data: users});
-        }).catch(error => {
-                alert(error)
-                localStorage.clear()
-                this.props.history.push({pathname: '/signin'})
-            }        
-        );
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log(`https://localhost:443/tasks/${window.localStorage.getItem("roomname")}`);
+        console.log(
+            `https://localhost:443/tasks/${window.localStorage.getItem(
+                "roomname"
+            )}`
+        );
         console.log(this.state);
         var body = { description: this.state.task };
-        fetch(`https://localhost:443/tasks/${window.localStorage.getItem("roomname")}`, {
-            method: "POST",
-            headers: {
-                "Authorization": window.localStorage.getItem("auth"),
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body),
-            mode: "cors",
-            cache: "default",
-        }).then(res => {
-            if (!res.ok) {
-                throw Error(res.statusText + " " + res.status);
+        fetch(
+            `https://localhost:443/tasks/${window.localStorage.getItem(
+                "roomname"
+            )}`,
+            {
+                method: "POST",
+                headers: {
+                    Authorization: window.localStorage.getItem("auth"),
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body),
+                mode: "cors",
+                cache: "default"
             }
-            return res.json();
-        }).then(data => {
-            console.log(data);
-            // this.props.socket.send(data)
-        }).catch(error => {
-                alert(error)
-                localStorage.clear()
-                this.props.history.push({pathname: '/signin'})
-            }
-        );
+        )
+            .then(res => {
+                if (!res.ok) {
+                    throw Error(res.statusText + " " + res.status);
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                // this.props.socket.send(data)
+            })
+            .catch(error => {
+                alert(error);
+                localStorage.clear();
+                this.props.history.push({ pathname: "/signin" });
+            });
     }
-
 
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
-                    <a className="navbar-brand" href="/main">To Do App</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <a className="navbar-brand" href="/main">
+                        To Do App
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarNavAltMarkup"
+                    >
                         <div className="navbar-nav">
-                            <a className="nav-item nav-link" href={"/main/" + localStorage.getItem("roomname").toLowerCase()}>Home</a>
-                            <a className="nav-item nav-link" href={"/admin/" + localStorage.getItem("roomname").toLowerCase()}>UserBoard</a>
+                            <a
+                                className="nav-item nav-link"
+                                href={
+                                    "/main/" +
+                                    localStorage
+                                        .getItem("roomname")
+                                        .toLowerCase()
+                                }
+                            >
+                                Home
+                            </a>
+                            <a
+                                className="nav-item nav-link"
+                                href={
+                                    "/admin/" +
+                                    localStorage
+                                        .getItem("roomname")
+                                        .toLowerCase()
+                                }
+                            >
+                                UserBoard
+                            </a>
                         </div>
                     </div>
-                    <button className="btn btn-warning my-2 my-sm-0 pull-right"
-                        onClick={(e => this.handleSignOut())}>
+                    <button
+                        className="btn btn-warning my-2 my-sm-0 pull-right"
+                        onClick={e => this.handleSignOut()}
+                    >
                         Sign Out
                     </button>
                 </nav>
@@ -150,10 +204,22 @@ export default class AddTaskView extends React.Component {
                                 </div>
                                 <form className="form-inline">
                                     <div className="form-group mx-sm-3 mb-2">
-                                        <input className="form-control" placeholder="Add Task"
-                                            onInput={evt => this.setState({ task: evt.target.value})} />
+                                        <input
+                                            className="form-control"
+                                            placeholder="Add Task"
+                                            onInput={evt =>
+                                                this.setState({
+                                                    task: evt.target.value
+                                                })
+                                            }
+                                        />
                                     </div>
-                                    <button className="btn btn-warning mt-2 mb-2 ml-2" onClick={(evt) => this.handleSubmit(evt)}>Submit</button>
+                                    <button
+                                        className="btn btn-warning mt-2 mb-2 ml-2"
+                                        onClick={evt => this.handleSubmit(evt)}
+                                    >
+                                        Submit
+                                    </button>
                                 </form>
                             </div>
                         </div>
